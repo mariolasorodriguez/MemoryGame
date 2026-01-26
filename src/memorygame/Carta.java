52405ba630093fd2cea0cdfeb1e5ac8ffa73d7eb
alvaro.vizuete <alvaro.vizuete@iesnervion.es>
2026-01-26 08:58:36 +0100

    Commit Clase carta

---
+++ src/memorygame/Carta.java
@@ -0,0 +1,38 @@
+package memorygame;
+
+public class Carta {
+
+	private int valor;
+	private boolean descubierta;
+
+	public Carta(int valor) {
+
+		this.valor = valor;
+
+		this.descubierta = false;
+	}
+
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+
+	public boolean isDescubierta() {
+		return descubierta;
+	}
+
+	public void setDescubierta(boolean descubierta) {
+		this.descubierta = descubierta;
+	}
+
+	public String toString() {
+		if (descubierta) {
+			return " " + valor + " ";
+		} else {
+			return " X ";
+		}
+	}
+}
